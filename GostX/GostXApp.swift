f406eb3c8709798cd6ba604e546d9a757116abbc
Kebin Liu <lkebin@gmail.com>
2022-06-16 19:59:17 +0800

    Initial Commit

---
+++ GostX/GostXApp.swift
@@ -0,0 +1,17 @@
+//
+//  GostXApp.swift
+//  GostX
+//
+//  Created by  on 2022/6/16.
+//
+
+import SwiftUI
+
+@main
+struct GostXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
