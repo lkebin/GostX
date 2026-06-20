6b0f4412e90be8391b8d6086104c343b0a18138a
Liu Kebin <lkebin@gmail.com>
2026-06-20 20:24:35 +0800

    Fix editor font color

--- android/app/src/main/kotlin/cn/liukebin/GostX/ui/config/ConfigScreen.kt
+++ android/app/src/main/kotlin/cn/liukebin/GostX/ui/config/ConfigScreen.kt
@@ -34,6 +34,8 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.sp
@@ -205,8 +207,10 @@ fun ConfigScreen(
                     .imePadding(),
                 textStyle = TextStyle(
                     fontFamily = FontFamily.Monospace,
-                    fontSize = 13.sp
+                    fontSize = 13.sp,
+                    color = MaterialTheme.colorScheme.onSurface
                 ),
+                cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface)
             )
         }
     }
